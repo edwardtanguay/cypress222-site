9e6defcaf5c7915accd72ff4cf9393bdbf7e1246
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-09 10:13:10 +0100

    message box is in

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+	const [message, setMessage] = useState('');
+
 	return (
 		<div className="App">
 			<h1>Info Site</h1>
 			<p>Welcome to this site.</p>
+			<input value={message} onChange={(e) => setMessage(e.target.value)}/>
+			<div className="showMessage">Your message has {message.length} characters.</div>
 		</div>
 	);
 }
